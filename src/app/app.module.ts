0f42a660fb4431fe82536105bf572c024167f48c
kadavaanil <kadavaanilkumar@gmail.com>
2022-12-14 19:20:03 +0530

    freshers jobs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JobsforFreshersComponent } from './jobsfor-freshers/jobsfor-freshers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JobsforFreshersComponent
   ],
   imports: [
     BrowserModule,
