95b50f2252f7dd002b9d0bd3a6dab46a591ca771
samba0320 <sambashivaraju0101@gmail.com>
2022-12-15 12:27:25 +0530

    Fixed icons

---
+++ src/app/recruitment-solution/recruitment-solution.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recruitment-solution',
+  templateUrl: './recruitment-solution.component.html',
+  styleUrls: ['./recruitment-solution.component.css']
+})
+export class RecruitmentSolutionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
