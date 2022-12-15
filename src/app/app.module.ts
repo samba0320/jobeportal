a3d46638778c6dcc6796fb37f9ec7f4344b9eddb
samba0320 <sambashivaraju0101@gmail.com>
2022-12-15 12:32:30 +0530

    Page not found component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RecruitmentSolutionComponent } from './recruitment-solution/recruitment-solution.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecruitmentSolutionComponent
+    RecruitmentSolutionComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
