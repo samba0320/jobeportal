ee4e0693c96215bb5ae2b2435e2fb992d4050c8b
fhgfghf <sureshyadav31842@gmail.com>
2022-12-19 10:40:47 +0530

    first  commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BusinessComponent } from './business/business.component';
+import { MarketingComponent } from './marketing/marketing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"" ,component:MarketingComponent,
+  },
+
+  {
+    path:"business",component:BusinessComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MarketingComponent } from './marketing/marketing.component';
+import {  RouterModule,Routes } from '@angular/router';
+import { BusinessComponent } from './business/business.component';
+
+
+const routes:Routes=[]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MarketingComponent,
+    BusinessComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
